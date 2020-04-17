a6413b7ca7b8e7688ffaeed3c6db72d7e3dc8cbe
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-17 20:32:54 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,5 @@ Rails.application.routes.draw do
   #   end
   
     resources :coupons, only: [:index, :new, :create]
-  get "coupons/:id", to: "coupons#show"
+  get "coupons/:id" => "coupons#show"
 end
