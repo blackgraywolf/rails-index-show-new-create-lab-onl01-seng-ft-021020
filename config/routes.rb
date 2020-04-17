30eae11e5325ede20da741af30ff854c5f711eaf
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-17 20:34:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+ get 'coupons/:id' => 'coupons#show'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
@@ -55,5 +55,5 @@ Rails.application.routes.draw do
   #   end
   
     resources :coupons, only: [:index, :new, :create]
-  get "coupons/:id" => "coupons#show"
+ 
 end
